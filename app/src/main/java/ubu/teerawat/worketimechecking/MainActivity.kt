d0231741c62589a22bec452804102fd65cfe3813
Teerawat Yamsai <nenoteerawat@gmail.com>
2020-06-11 07:25:28 +0700

    init project.

---
+++ app/src/main/java/ubu/teerawat/worketimechecking/MainActivity.kt
@@ -0,0 +1,30 @@
+package ubu.teerawat.worketimechecking
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.refresh -> {
+                Toast.makeText(this, "Already Refresh", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
